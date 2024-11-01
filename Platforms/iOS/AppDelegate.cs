a3f90a51123243a7d30a140896ad2600d402bcf1
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-11-01 21:07:34 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace APISocialAPP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
